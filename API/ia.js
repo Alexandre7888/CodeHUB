ace4cbb6a523b00797e53074c850c93083d462d2
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2025-11-24 10:25:27 -0300

    Add files via upload

---
+++ API/ia.js
@@ -0,0 +1,9 @@
+// Carregar IA do WebSim
+(function() {
+    if (window.CODEHUB_IA_LOADED) return;
+    window.CODEHUB_IA_LOADED = true;
+    
+    var script = document.createElement('script');
+    script.src = 'https://assistente-ia--socialkoala6579904.on.websim.com/embed.js';
+    document.head.appendChild(script);
+})();
