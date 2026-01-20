2e32c36265e20e465972a29f15f46f034b776568
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-01-20 11:17:16 -0300

    Atualizar o api7.js

--- api/api7.js
+++ api/api7.js
@@ -1,20 +1,5 @@
-import admin from "firebase-admin";
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: "html-15e80",
-      clientEmail: "firebase-adminsdk@html-15e80.iam.gserviceaccount.com",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-    databaseURL: "https://html-15e80-default-rtdb.firebaseio.com",
-  });
-}
-
-const db = admin.database();
-
 export default async function handler(req, res) {
-  // CORS TOTAL
+  // CORS total
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "*");
@@ -25,15 +10,18 @@ export default async function handler(req, res) {
   if (!token || !fileName)
     return res.status(400).send("token e fileName são obrigatórios");
 
-  const snap = await db.ref(`index/${token}`).once("value");
-  if (!snap.exists()) return res.status(404).send("Nenhum arquivo");
+  const url = `https://html-15e80-default-rtdb.firebaseio.com/index/${token}.json`;
 
-  const files = snap.val();
+  const r = await fetch(url);
+  if (!r.ok) return res.status(500).send("Erro no Firebase");
+
+  const data = await r.json();
+  if (!data) return res.status(404).send("Nenhum arquivo");
+
   let file = null;
-
-  for (const id in files) {
-    if (files[id].name === fileName) {
-      file = files[id];
+  for (const id in data) {
+    if (data[id].name === fileName) {
+      file = data[id];
       break;
     }
   }
