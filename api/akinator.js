e869c36690d92ebed594603c6998fe096d0721a4
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-01-21 23:24:56 -0300

    Atualizar o akinator.js

--- api/akinator.js
+++ api/akinator.js
@@ -1,5 +1,4 @@
 // api/akinator.js
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 let session = null; // mantém estado temporário
 
@@ -9,16 +8,19 @@ module.exports = async (req, res) => {
   try {
     if (action === 'start') {
       // Iniciar jogo
-      const init = await fetch('https://srv12.akinator.com:9162/new_session?callback=foo&partner=1&player=website&uid=&frontaddr=127.0.0.1&constraint=ETAT%3C%3E%22%22&lang=pt', {
-        headers: { 'Accept': '*/*' }
-      });
+      const init = await fetch(
+        'https://srv12.akinator.com:9162/new_session?callback=foo&partner=1&player=website&uid=&frontaddr=127.0.0.1&constraint=ETAT%3C%3E%22%22&lang=pt'
+      );
+
       const text = await init.text();
       const json = JSON.parse(text.match(/foo\((.*)\)/)[1]);
+
       session = {
         session: json.parameters.identification.session,
         signature: json.parameters.identification.signature,
         step: 0
       };
+
       return res.json({ question: json.parameters.step_information.question });
     }
 
@@ -27,8 +29,7 @@ module.exports = async (req, res) => {
       if (answer === undefined) return res.status(400).json({ error: "Informe a resposta: 0-4" });
 
       const stepReq = await fetch(
-        `https://srv12.akinator.com:9162/answer?callback=foo&session=${session.session}&signature=${session.signature}&step=${session.step}&answer=${answer}`,
-        { headers: { 'Accept': '*/*' } }
+        `https://srv12.akinator.com:9162/answer?callback=foo&session=${session.session}&signature=${session.signature}&step=${session.step}&answer=${answer}`
       );
 
       const textStep = await stepReq.text();
