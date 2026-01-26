08ccdcbb103169f632207aaab502d260fc54924d
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-01-26 16:44:24 -0300

    Atualizar o whatsapp.js

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -1,14 +1,16 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Use POST" });
+  }
 
   const { numero, mensagem } = req.body;
 
   const response = await fetch(
-    "https://graph.facebook.com/v19.0/SEU_PHONE_NUMBER_ID/messages",
+    `https://graph.facebook.com/v19.0/${process.env.WHATSAPP_PHONE_ID}/messages`,
     {
       method: "POST",
       headers: {
-        "Authorization": "Bearer EAAUHGYqLaqMBQkCyY2EvodHtK5ZCh4EPrpqKYv6LpBL7j6yBwaEj24QaSGDOVjjpt5jwKxE9W3hpY53OSxvg5YRxIajxRGWDJZCfAllg9gqMcCuo8KTWYuTwt8mVrhDG0hQSSZA6nC352ZB1xdNtyXgdXlmAEST7IEGYqufkVlrBNgieYKSe8YD52MwOPTXG9LYr8kxZAsYMGu6yFTDblhH2OkH95xwtRRoPhmVjKZA3uzh5ZA3ukoZAo5YWcByZCY9hR9xZAi9ZA0QcljgizImpRGD1ybd",
+        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
@@ -21,5 +23,5 @@ export default async function handler(req, res) {
   );
 
   const data = await response.json();
-  res.status(200).json(data);
+  return res.status(200).json(data);
 }
