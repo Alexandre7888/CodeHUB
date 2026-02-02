0c632004dc50a40586d79b752235b5bf9215898c
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-02-02 12:44:28 -0300

    Atualizar o view.js

--- api/view.js
+++ api/view.js
@@ -1,21 +1,14 @@
-export const config = {
-  runtime: 'nodejs'
-};
-
-const { list } = require('@vercel/blob');
-
 module.exports = async (req, res) => {
   try {
-    const data = await list({
-      token: process.env.BLOB_READ_WRITE_TOKEN
+    const r = await fetch('https://api.vercel.com/v2/blob', {
+      headers: {
+        Authorization: `Bearer ${process.env.BLOB_READ_WRITE_TOKEN}`
+      }
     });
 
-    res.status(200).json(data.blobs);
-  } catch (err) {
-    console.error('BLOB ERROR:', err);
-    res.status(500).json({
-      error: 'Blob falhou',
-      message: err.message
-    });
+    const data = await r.json();
+    res.status(200).json(data.blobs || data);
+  } catch (e) {
+    res.status(500).json({ erro: e.message });
   }
 };
