9b6b12334d53773fce4b5cfe630f4faa68f7b896
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-02-22 23:33:13 -0300

    Atualizar o account.js

--- api/account.js
+++ api/account.js
@@ -1,13 +1,32 @@
 export default async function handler(req, res) {
-  const targetUrl = 'https://script.google.com/macros/s/AKfycbxKCEdhxFayijLW3GafXV5yN2dZ-vUeR4AcZdrpjeGT1eThFDBLlYF_C6wcpYa9Lmyv7w/exec';
+  // Configurar CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  const baseUrl = 'https://script.google.com/macros/s/AKfycbxKCEdhxFayijLW3GafXV5yN2dZ-vUeR4AcZdrpjeGT1eThFDBLlYF_C6wcpYa9Lmyv7w/exec';
   
   try {
-    const response = await fetch(targetUrl);
-    const data = await response.text();
+    // Construir URL com os parâmetros da requisição
+    const url = new URL(baseUrl);
     
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Pegar todos os parâmetros da URL (action, uid, etc)
+    Object.keys(req.query).forEach(key => {
+      url.searchParams.append(key, req.query[key]);
+    });
+
+    // Fazer a requisição para o Google Apps Script
+    const response = await fetch(url.toString(), {
+      method: req.method,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    // Retornar a resposta
+    const data = await response.text();
     res.status(response.status).send(data);
+    
   } catch (error) {
+    console.error('Erro:', error);
     res.status(500).send('Erro no proxy');
   }
 }
