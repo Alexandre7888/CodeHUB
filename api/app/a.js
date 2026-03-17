4a0361ba9a6fed5332d825d90df2209949bf1a24
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-03-16 22:13:53 -0300

    Criar app/a.js

---
+++ api/app/a.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+
+  // bloquear acesso direto
+  const origin = req.headers.origin
+
+  if (origin !== "https://code.codehub.ct.ws") {
+    res.status(403).send("<h1>Erro</h1>")
+    return
+  }
+
+  res.setHeader("Access-Control-Allow-Origin", "https://code.codehub.ct.ws")
+
+  res.json({
+    token: process.env.PUTER_TOKEN
+  })
+}
