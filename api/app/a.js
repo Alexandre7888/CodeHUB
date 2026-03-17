ac253acaa5ac16e0f5aa8c2c35f355c9ab0033ca
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-03-16 22:23:57 -0300

    Atualizar o a.js

--- api/app/a.js
+++ api/app/a.js
@@ -1,16 +1,19 @@
 export default function handler(req, res) {
 
-  // bloquear acesso direto
-  const origin = req.headers.origin
+  const origin = req.headers.origin;
 
+  // bloquear se não for seu site
   if (origin !== "https://code.codehub.ct.ws") {
-    res.status(403).send("<h1>Access denied </h1>")
-    return
+    res.status(403).send("<h1>Access denied.</h1>");
+    return;
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "https://code.codehub.ct.ws")
+  // permitir apenas seu domínio
+  res.setHeader("Access-Control-Allow-Origin", "https://code.codehub.ct.ws");
+  res.setHeader("Content-Type", "application/json");
 
-  res.json({
-    token: process.env.PUTER_TOKEN
-  })
+  res.status(200).json({
+    access: process.env.PUTER_TOKEN
+  });
+
 }
