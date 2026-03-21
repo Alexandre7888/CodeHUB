a2514fcb725f798f0106394c5d92464643b4c268
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-03-21 19:23:20 -0300

    Excluir list.js

--- api/lista/list.js
+++
@@ -1,9 +0,0 @@
-const { list } = require('@vercel/blob');
-
-module.exports = async (req, res) => {
-  const data = await list({
-    token: process.env.BLOB_READ_WRITE_TOKEN
-  });
-
-  res.json(data.blobs);
-};
